46f8d4f28191b32d6f7fa3040d4cf91ead9f5139
tokechan <fleatoke@gmail.com>
2025-05-01 17:51:25 +0900

    Add:MCP demo

---
+++ api/src/index.ts
@@ -0,0 +1,19 @@
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.get('systems/ping', (c) => {
+  return c.json({ message: "pong" });
+});
+
+serve({
+  fetch: app.fetch,
+  port: 3000
+}, (info) => {
+  console.log(`Server is running on http://localhost:${info.port}`)
+})
